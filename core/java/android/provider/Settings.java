62cad148c06c6e55aa02261210a6e977a45987c3
elpaablo <donopwhile1@gmail.com>
2023-12-16 11:38:37 +0000

    Add SETTINGS_STYLE to Settings

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6550,6 +6550,10 @@ public final class Settings {
          */
         public static final String UDFPS_ICON = "udfps_icon";
 
+        /**
+         * @hide
+         */
+        public static final String SETTINGS_STYLE = "settings_style";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
