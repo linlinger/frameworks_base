6afa64c133f4d10f9ddc33d574f1467912df80b6
elpaablo <donopwhile1@gmail.com>
2023-12-18 06:18:04 +0000

    Revert "SystemUI: Add QS Panel Style [1/3]"
    This reverts commit 1f58c1004257cbfae4b53b5e701f2831a0256896.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6131,11 +6131,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String QS_PANEL_STYLE = "qs_panel_style";
-
-        /**
-         * @hide
-         */
         public static final String QQS_LAYOUT_ROWS_LANDSCAPE = "qqs_layout_rows_landscape";
 
         /**
